82baff595fd33f408bd9db41becbf660d8502405
smelc <smelc@users.noreply.github.com>
2020-03-02 22:45:13 +0100

    Update README.md and mybash.sh

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # myconfig
+
 Personal configuration files such as .vimrc
+
+# Tips and tricks
+
+* Generate strong password: `apg -a 0 -s -m 48`
--- mybash.sh
+++ mybash.sh
@@ -5,5 +5,10 @@
 
 ## Nix
 # To use with nix-env --switch-profile $NIX_USER_PROFILE_DIR/name
-export NIX_USER_PROFILE_DIR="/nix/var/nix/profiles/per-user/churlin/"
+# export NIX_USER_PROFILE_DIR="/nix/var/nix/profiles/per-user/churlin/"
 # What is my current profile? ls -l ~/.nix-profile
+
+. /home/churlin/.nix-profile/etc/profile.d/nix.sh
+
+# https://direnv.net/docs/hook.html (from https://github.com/target/lorri)
+eval "$(direnv hook bash)"
