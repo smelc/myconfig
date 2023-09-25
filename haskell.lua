0c3463446724521c2cb0ba2da1f7c6e7adf1326e
Clément Hurlin <smelc@users.noreply.github.com>
2023-09-25 12:14:42 +0200

    Enhancements to nvim Haskell setup

--- haskell.lua
+++ haskell.lua
@@ -3,23 +3,8 @@
 -- From Mateuzs:
 -- https://gist.github.com/carbolymer/f0e10a3bbd07a022f8eccf1c52e65356
 
--- local local_on_attach = function(client, bufnr)
---   -- Enable completion triggered by <c-x><c-o>
---   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
---   local bufopts = vim.tbl_extend('keep', def_opts, { buffer = bufnr })
---   keymap('n', '<leader>k', vim.diagnostic.open_float, bufopts)
---   keymap('n', 'gd', tb.lsp_definitions, bufopts)
---   keymap('n', '<leader>la', vim.lsp.codelens.run, bufopts)
---   keymap('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
---   keymap('n', '<leader>F', function() vim.lsp.buf.format {async = true} end, bufopts)
---   keymap('v', '<leader>F', vim.lsp.buf.format, bufopts)
---   keymap('n', '<leader>ls', tb.lsp_document_symbols, bufopts)
---   keymap('n', '<leader>lw', tb.lsp_workspace_symbols, bufopts)
---   keymap('n', '<leader>lq', tb.lsp_dynamic_workspace_symbols, bufopts)
---   keymap('n', '<leader>lo', '<cmd>:SymbolsOutline<CR>', bufopts)
-
--- end
+local telescope = require 'telescope'
+telescope.load_extension('hoogle')
 
 local tb = require 'telescope.builtin'
 local ht = require('haskell-tools')
@@ -33,8 +18,9 @@ ht.start_or_attach {
       -- haskell-language-server relies heavily on codeLenses,
       -- so auto-refresh (see advanced configuration) is enabled by default
       vim.keymap.set('n', '<space>ca', vim.lsp.codelens.run, opts)
+      -- vim.keymap.set('n', '<leader>k', vim.diagnostic.open_float, bufopts) Doesn't work :-(
+      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
 
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
       vim.keymap.set('n', '<leader>go', tb.lsp_outgoing_calls, bufopts)
       vim.keymap.set('n', '<leader>gi', tb.lsp_incoming_calls, bufopts)
       vim.keymap.set('n', '<leader>rf', tb.lsp_references, bufopts)
@@ -42,14 +28,18 @@ ht.start_or_attach {
       -- vscode style
       vim.keymap.set('n', '<F8>', vim.diagnostic.goto_next, bufopts)
       vim.keymap.set('n', '<Shift-F8>', vim.diagnostic.goto_prev, bufopts)
+      vim.keymap.set('n', '<F12>', tb.lsp_definitions, bufopts)
 
-      vim.keymap.set('n', '<leader>ac', vim.lsp.buf.code_action, bufopts)
+      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
 
       -- haskell-tools specific
       vim.keymap.set('n', '<space>hs', ht.hoogle.hoogle_signature, opts)
-      vim.keymap.set('n', '<space>ea', ht.lsp.buf_eval_all, opts)
+      -- vim.keymap.set('n', '<space>ea', ht.lsp.buf_eval_all, opts)
     end,
-    cmd = { 'haskell-language-server', '--lsp' }
+    cmd = { 'haskell-language-server', '--lsp' },
+    default_settings = {
+      formattingProvider = "stylish",
+    },
   },
 }
 
--- init.lua
+++ init.lua
@@ -14,6 +14,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
     "mfussenegger/nvim-jdtls",
     "joshdick/onedark.vim", -- onedark
+    "EdenEast/nightfox.nvim", -- nightfox
     "tpope/vim-commentary", -- gc to comment a line
     "vim-airline/vim-airline", -- nice status bar
     "junegunn/fzf",
@@ -23,10 +24,16 @@ require("lazy").setup({
     "mfussenegger/nvim-dap", -- for haskell-tools
     { "mrcjkb/haskell-tools.nvim", branch = "1.x.x" },
 
+    "luc-tielen/telescope_hoogle",
+    "nvim-treesitter/nvim-treesitter", -- recommended by haskell-tools
+    "neovimhaskell/haskell-vim", -- better highlighting in haskell
+
     "neovim/nvim-lspconfig",
+
+    "BurntSushi/ripgrep", -- for telescope.nvim
 })
 
-vim.cmd.colorscheme("onedark")
+vim.cmd.colorscheme("nightfox")
 
 -- https://neovim.io/doc/user/lua-guide.html#lua-guide-using-Lua
 
@@ -38,7 +45,13 @@ vim.api.nvim_set_keymap('n', '<S-l>', "<cmd>:tabn<CR>", {})
 vim.api.nvim_set_keymap('n', '<S-h>', "<cmd>:tabp<CR>", {})
 
 -- Alias :FZF to Ctrl-P (vscode style)
-vim.api.nvim_set_keymap('n', '<C-p>', "<cmd>:FZF<CR>", {})
+-- vim.api.nvim_set_keymap('n', '<C-p>', "<cmd>:FZF<CR>", {})
+
+local tb = require('telescope.builtin')
+vim.keymap.set('n', '\\ff', tb.find_files, {})
+vim.keymap.set('n', '\\fg', tb.live_grep, {})
+
+-- vim.api.nvim_set_keymap('n', '<leader-g>', "<cmd>:Grepper -tool ag<CR>", {})
 
 -- Do not autoreload files on change, ask for confirmation instead
 vim.opt.autoread = false
